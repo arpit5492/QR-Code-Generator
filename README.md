46ce700d5ecfebcfbc1939b71f70804c08d42eb1
arpit5492 <arpitmallick99@gmail.com>
2024-01-17 16:25:25 -0800

    Updated readme.md file

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-## QR Code Generator
+# QR Code Generator
+
+## Overview
+This project is a Node.js application using Express.js. It allows users to input a URL, which the application then converts into a QR code image. Additionally, the entered URL is saved in a separate file for record-keeping.
+
+## Getting Started
+
+### Prerequisites
+- Node.js installed on your machine.
+
+### Installation
+1. Clone the repository to your local machine:
+   ```
+   git clone https://github.com/arpit5492/QR-Code-Generator.git
+   ```
+
+2. To install all the dependecies:
+   ```
+   npm install
+   ```
+3. Start the application by executing:
+   ```
+   node index.js
+   ```
+
+
--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
 import inquirer from "inquirer"
 import qr from "qr-image"
 import fs from "fs"
 inquirer
   .prompt([
-    //Use the inquirer npm package to get user input.
     {message:"Type your URL here: ",
      name:"URL"
     }
   ])
   .then((answers) => {
-    //Use the qr-image npm package to turn the user entered URL into a QR code image.
     const url = answers.URL;
     let qr_image = qr.image(url, {type: 'png'});
     qr_image.pipe(fs.createWriteStream('my_qr_image.png'));
 
-    //Create a txt file to save the user input using the native fs node module.
     fs.writeFile("QR-code.txt", url, function(err){
         if(err) throw err;
         console.log("The file has been successfully saved!!");
@@ -28,8 +20,7 @@ inquirer
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
     } else {
-      // Something else went wrong
+      
     }
   });
